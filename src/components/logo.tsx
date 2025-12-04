43bcb210f3396d5845c28b5a1689746afd7c1bdf
logonotobscurity <logooluwamayowa@protonmail.com>
2025-12-03 22:50:35 -0800

    Update: Change logo to new URL in header and footer

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -7,7 +7,7 @@ export function Logo({ className }: { className?: string }) {
     <Link href="/" className={cn("relative block aspect-[350/174]", className)} aria-label="BZION Home">
       <h2 className="sr-only">BZION Hub</h2>
       <Image 
-        src="https://i.ibb.co/tpzn5Cpd/Main-Logo-removebg-preview-1.png" 
+        src="https://i.ibb.co/1tfpsbFT/logo-2.png" 
         alt="Bzion Hub Logo"
         fill
         sizes="(max-width: 768px) 120px, 162px"
